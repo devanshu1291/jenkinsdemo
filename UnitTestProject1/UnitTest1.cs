c0727c9bb5bf9be907d26a1172816f40e70730ea
Devanshu Goyal <devanshu.goyal@nagarro.com>
2021-07-14 21:11:29 +0530

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApplication1;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		Calculator _calc;
+
+		public  UnitTest1()
+		{
+			_calc = new Calculator();
+		}
+		[TestMethod]
+		public void shouldaddtwonumbers()
+		{
+			int res = _calc.Add(5, 3);
+			Assert.AreEqual(res, 8);
+		}
+		[TestMethod]
+		public void shouldsubtwonumbers()
+		{
+			int res = _calc.Sub(5, 3);
+			Assert.AreEqual(res, 2);
+		}
+	}
+}
---
+++ WebApplication1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+	public class Calculator
+	{
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public int Sub(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
